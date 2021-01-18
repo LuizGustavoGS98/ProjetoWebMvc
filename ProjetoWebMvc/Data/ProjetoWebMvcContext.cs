7673caa79bda6dd8a7e78092fd43f1975af32058
LuizGustavoGS98 <luiz.silva@bandtec.com.br>
2021-01-18 02:17:27 -0300

    Cadastro funcionando sem departamento

--- ProjetoWebMvc/Data/ProjetoWebMvcContext.cs
+++ ProjetoWebMvc/Data/ProjetoWebMvcContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProjetoWebMvc.Models;
 
 namespace ProjetoWebMvc.Data
 {
@@ -9,6 +10,8 @@ namespace ProjetoWebMvc.Data
         {
         }
 
-        public DbSet<ProjetoWebMvc.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
---
+++ ProjetoWebMvc/Services/SellerService.cs
@@ -0,0 +1,28 @@
+﻿using ProjetoWebMvc.Data;
+using ProjetoWebMvc.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetoWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly ProjetoWebMvcContext _context;
+
+        public SellerService(ProjetoWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+    }
+}
